37f496265a0a46a07db418a390883f8c743b415a
whg0130 <62163330+whg0130@users.noreply.github.com>
2022-04-04 12:22:58 +0800

    rr

--- empApp.py
+++ empApp.py
@@ -22,7 +22,7 @@ table = 'Employee'
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('homepage.html')
 
 @app.route("/displayEmp")
 def displayEmp():
